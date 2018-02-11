17c2ff56a6c2ab84bc282c649598680be2e56d6f
John Doe <javadoc.com>
2018-02-12 00:29:15 +0200

    Initial commit

---
+++ src/main/java/com/ishe/Clients.java
@@ -0,0 +1,47 @@
+package com.ishe;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+/**
+ * Created by igor on 25.10.2017.
+ */
+public class Clients {
+    public static void showClients(Connection conn) throws SQLException {
+        try(PreparedStatement pst = conn.prepareStatement("SELECT * FROM Clients")) {
+            try (ResultSet rs = pst.executeQuery()) {
+                Ordering.displayMetadata(rs);
+            }
+        }
+    }
+
+    public static void addClients(Connection conn) throws SQLException {
+        conn.setAutoCommit(false);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter client's name:");
+        String name = scan.nextLine();
+        System.out.println("Now enter client's address:");
+        String address = scan.nextLine();
+        System.out.println("Now enter client's phone number:");
+        String phone = scan.nextLine();
+        System.out.println("Finally enter client's credit card number:");
+        String creditcard = scan.nextLine();
+        String query = "INSERT INTO Clients (name, address, phone, creditcard) VALUES(?, ?, ?, ?)";
+        try (PreparedStatement ps = conn.prepareStatement(query)) {
+            ps.setString(1, name);
+            ps.setString(2, address);
+            ps.setString(3, phone);
+            ps.setString(4, creditcard);
+            ps.executeUpdate();
+            conn.commit();
+        } catch (Exception e){
+            e.printStackTrace();
+            conn.rollback();
+        } finally{
+            conn.setAutoCommit(true);
+        }
+    }
+}
